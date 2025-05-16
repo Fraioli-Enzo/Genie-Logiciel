e61ae7e013cc030e6088c6628db62c97dbe70c0f
Enzo FRAIOLI <enzo.fraioli@viacesi.fr>
2025-05-16 09:19:00 +0200

    Add interface

---
+++ App/WpfApp1/AddBackup.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour AddBackup.xaml
+    /// </summary>
+    public partial class AddBackup : Page
+    {
+        public AddBackup()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TextBoxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBoxSource_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBoxTarget_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSource_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonTarget_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonFull_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonDifferential_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ App/WpfApp1/Settings.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour Settings.xaml
+    /// </summary>
+    public partial class Settings : Page
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonFR_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonEN_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonXML_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonJSON_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
